eb4c60c6d35ede5c1978b4f5e12d288b25044051
Lynn James <6905929+lynnj23@users.noreply.github.com.>
2025-03-17 15:05:26 +0000

    Updated the project, formated dataset inputs and tokenised the test data
    Added in LoRa model from transformers and set it up. yet to be trained

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ then output a table of learned results."""
 # Importing areas
 import torch
 from datasets import load_dataset, concatenate_datasets
-
 # Step 2 import the tokenizer
-from transformers import AutoTokenizer as att
+from transformers import AutoModelForSequenceClassification, AutoTokenizer as att
 
 # import pandas as pd
 
@@ -15,14 +14,14 @@ from transformers import AutoTokenizer as att
 SPLIT_TRAINING = "1%"
 TEST_DATA = "1%"
 
-# Import the chosen model, in this instance I have taken recommendation to stick with LoRa
+# Import the chosen model; in this instance I have taken recommendation to stick with LoRa
 
 
-# Import the chosen labelled dataset from Huggingface.
+# Import the chosen labeled dataset from Huggingface.
 
 train_dataset = load_dataset("cirimus/super-emotion",
                              split=f"train[:{SPLIT_TRAINING}]")  # for testing purposes load first 1%. \
-# Stream or slice later dependent on the accuracy of th training.
+# Stream or slice later dependent on the accuracy of the training.
 test_dataset = load_dataset("cirimus/super-emotion", split=f"test[:{TEST_DATA}]")  # this loads the test dataset
 
 train_dataset = train_dataset.add_column("split", ["train"] * len(train_dataset))
@@ -34,7 +33,7 @@ combined_dataset = concatenate_datasets([train_dataset, test_dataset])
 # print(f"test#5 This is the train set: {test_dataset}")
 
 df = combined_dataset.to_pandas()
-# print first 5 rows for visual check
+# print the first 5 rows for visual check
 print(df.head(5))
 
 for entry in combined_dataset.select(range(3)):
@@ -82,7 +81,7 @@ def preprocess_function(examples):
             tokenized = tokenizer(txt, truncation=True, padding="max_length")
             token_dataset.append(tokenized["input_ids"])
         else:
-            # Tokenising non-test data here. This is a bit ugly, but I have got myself in a twist here at moment
+            # Tokenising non-test data here. This is a bit ugly, but I have got myself in a twist here at a moment
             tokenized = tokenizer(txt, truncation=True, padding="max_length")
             token_dataset.append(tokenized["input_ids"])
     return {"input_ids": token_dataset}
@@ -96,9 +95,31 @@ for entry in tokenized_dataset.select(range(1)):
     print(entry["input_ids"])
 
 # Load the PEFT Model and run through steps to train chosen model
+my_model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased",
+                                                           num_labels=7, id2label={0: "NEUTRAL",
+                                                                                   1: "SURPRISE",
+                                                                                   2: "FEAR",
+                                                                                   3: "SADNESS",
+                                                                                   4: "JOY",
+                                                                                   5: "ANGER",
+                                                                                   6: "LOVE"
+                                                                                   }, label2id={"NEUTRAL": 0,
+                                                                                                "SURPRISE": 1,
+                                                                                                "FEAR": 2,
+                                                                                                "SADNESS": 3,
+                                                                                                "JOY": 4,
+                                                                                                "ANGER": 5,
+                                                                                                "LOVE": 6
+                                                                                                })
 
+# Freeze all the parameters of the base model
+for param in my_model.base_model.parameters():
+    param.requires_grad = False
+
+my_model.classifier
+print(f"Test#8 My model output: {my_model}")
 
 # Load the trained model
 
 
-# Output the text classification results in table format comparing before and after outputs.
+# The Output text classification results in table format comparing before and after outputs.
